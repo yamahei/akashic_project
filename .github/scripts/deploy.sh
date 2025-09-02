2e4fdf8e468c3bb786c3d6c19ac709e83b055023
yamahei <yamaorii@gmail.com>
2025-09-03 00:00:23 +0900

    fix: `publish.sh`のロジックをより忠実に再現するよう修正

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,77 +2,98 @@
 
 set -eu
 
+ROOTDIR=$(pwd)
 PROJECTROOT=./prj
-PUBLISHDIR=./publish
-PROJECTINFO=./prj-info.json
+PUBLISHDIR=${ROOTDIR}/publish
+PROJECTINFO="${ROOTDIR}/prj-info.json"
 
-# 出力用ディレクトリを初期化
-rm -rf ${PUBLISHDIR}
-mkdir -p ${PUBLISHDIR}
+# 関数定義
+proc_in_prj () {
+    local PJDIR_REL=$1 # ex: ./prj/00.hello-akashic/
+    local PJDIR_ABS=${ROOTDIR}/${PJDIR_REL}
 
-# トップレベルのREADME.mdをhtmlに変換
-if [ -f README.md ]; then
-    npm install -g markdown-to-html-cli
-    markdown-to-html-cli --output ${PUBLISHDIR}/index.html --style=https://unpkg.com/mvp.css
-fi
+    cd "${PJDIR_ABS}"
+    echo "  - Processing in ${PJDIR_REL}"
 
-# prj-info.jsonを初期化
-echo "{}" > ${PROJECTINFO}
-
-# prj/配下のディレクトリをループ処理
-for PROJECTDIR in ${PROJECTROOT}/*/; do
-    echo "Processing ${PROJECTDIR}"
-    CURRDIR=$(basename ${PROJECTDIR})
-    if [ ! -d "${PROJECTDIR}" ]; then continue; fi
-    if [ ${CURRDIR} = "assets" ]; then continue; fi
-    if [ ${CURRDIR} = "lib" ]; then continue; fi
-
-    # プロジェクト配下での処理
-    cd "${PROJECTDIR}"
-
-    # prj-info.jsonにプロジェクト情報を記録
-    cat ${PROJECTINFO} | jq --arg dir "${CURRDIR}" '. + {($dir): {}}' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
+    # `prj-info.json`にプロジェクトのキーを作成
+    jq --arg dir "${PJDIR_REL}" '.[$dir] = []' < "${PROJECTINFO}" > "${PROJECTINFO}.tmp" && mv "${PROJECTINFO}.tmp" "${PROJECTINFO}"
 
     # index.htmlがあればコピー
     if [ -f index.html ]; then
         echo "  - Processing index.html"
-        cp index.html ../../${PUBLISHDIR}/${CURRDIR}.html
-        cat ${PROJECTINFO} | jq --arg key "${CURRDIR}" --arg value "${CURRDIR}.html" '.[$key].url = $value' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
+        # `prj-info.json`に記録
+        jq --arg key "${PJDIR_REL}" --arg value "${PJDIR_REL}index.html" '.[$key] += [$value]' < "${PROJECTINFO}" > "${PROJECTINFO}.tmp" && mv "${PROJECTINFO}.tmp" "${PROJECTINFO}"
     fi
 
     # README.mdがあればHTMLに変換
     if [ -f README.md ]; then
         echo "  - Processing README.md"
-        TITLE=$(head -n 1 README.md | sed 's/# //')
-        cat ${PROJECTINFO} | jq --arg key "${CURRDIR}" --arg value "${TITLE}" '.[$key].title = $value' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
-        if [ ! -f index.html ]; then # index.htmlがなければREADMEをトップにする
-            markdown-to-html-cli --output ../../${PUBLISHDIR}/${CURRDIR}.html --style=https://unpkg.com/mvp.css
-            cat ${PROJECTINFO} | jq --arg key "${CURRDIR}" --arg value "${CURRDIR}.html" '.[$key].url = $value' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
-        fi
+        local TITLE=$(head -n 1 README.md)
+        # `prj-info.json`にタイトルを記録
+        jq --arg key "${PJDIR_REL}" --arg value "#${TITLE}" '.[$key] += [$value]' < "${PROJECTINFO}" > "${PROJECTINFO}.tmp" && mv "${PROJECTINFO}.tmp" "${PROJECTINFO}"
+        
+        # HTMLに変換してpublishディレクトリに配置
+        markdown-to-html-cli --output "${PUBLISHDIR}/$(basename ${PJDIR_REL}).html" --style=https://unpkg.com/mvp.css
+        
+        # `prj-info.json`にHTMLファイルへのパスを記録
+        jq --arg key "${PJDIR_REL}" --arg value "./publish/$(basename ${PJDIR_REL}).html" '.[$key] += [$value]' < "${PROJECTINFO}" > "${PROJECTINFO}.tmp" && mv "${PROJECTINFO}.tmp" "${PROJECTINFO}"
     fi
 
     # game.jsonがあればakashic export
     if [ -f game.json ]; then
         echo "  - Processing game.json"
-        OUTDIR=../../${PUBLISHDIR}/${CURRDIR}
+        local OUTDIR=${PUBLISHDIR}/$(basename ${PJDIR_REL})
+        
         npm install
+        npm run build
         akashic export html --magnify --output "${OUTDIR}" --force
-        cat ${PROJECTINFO} | jq --arg key "${CURRDIR}" --arg value "${CURRDIR}/index.html" '.[$key].url = $value' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
-        if [ -z "$(cat ${PROJECTINFO} | jq '.[$key].title')" ]; then
-            TITLE=$(cat game.json | jq -r '.name')
-            cat ${PROJECTINFO} | jq --arg key "${CURRDIR}" --arg value "${TITLE}" '.[$key].title = $value' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
-        fi
+        
+        # `prj-info.json`に記録
+        jq --arg key "${PJDIR_REL}" --arg value "./publish/$(basename ${PJDIR_REL})/index.html" '.[$key] += [$value]' < "${PROJECTINFO}" > "${PROJECTINFO}.tmp" && mv "${PROJECTINFO}.tmp" "${PROJECTINFO}"
     fi
 
     # png画像があれば背景画像用にパスを記録
     IMGFILE=$(ls -1 *.png | head -n 1 || true)
     if [ -f "${IMGFILE}" ]; then
-        cp ${IMGFILE} ../../${PUBLISHDIR}/
-        cat ${PROJECTINFO} | jq --arg key "${CURRDIR}" --arg value "${IMGFILE}" '.[$key].image = $value' > ../.jqtmp && mv ../.jqtmp ${PROJECTINFO}
+        # `prj-info.json`に記録
+        jq --arg key "${PJDIR_REL}" --arg value "${PJDIR_REL}${IMGFILE}" '.[$key] += [$value]' < "${PROJECTINFO}" > "${PROJECTINFO}.tmp" && mv "${PROJECTINFO}.tmp" "${PROJECTINFO}"
     fi
 
-    cd ../..
+    cd "${ROOTDIR}"
+}
+
+# --- 実処理 ---
+
+# 出力用ディレクトリを初期化
+echo "Preparing ${PUBLISHDIR}"
+rm -rf ${PUBLISHDIR}
+mkdir -p ${PUBLISHDIR}
+
+# トップレベルのREADME.mdをhtmlに変換
+markdown-to-html-cli --output ${PUBLISHDIR}/README.html --style=https://unpkg.com/mvp.css
+
+# `prj-info.json`を初期化
+echo "{}" > "${PROJECTINFO}"
+
+# プロジェクトDIR内の作業(`prj/`直下のディレクトリでループする)
+for PROJECTDIR_REL in ${PROJECTROOT}/*/; do
+    echo "Processing ${PROJECTDIR_REL}"
+    CURRDIR=$(basename ${PROJECTDIR_REL})
+    if [ ! -d "${PROJECTDIR_REL}" ]; then continue; fi
+    if [ ${CURRDIR} = "assets" ]; then continue; fi
+    if [ ${CURRDIR} = "lib" ]; then continue; fi
+
+    proc_in_prj "${PROJECTDIR_REL}"
 done
 
-# prj-info.jsonをpublishディレクトリに移動
-mv ${PROJECTINFO} ${PUBLISHDIR}/
+# 全てのシンボリックリンクを削除
+if [ -n "$(find . -type l)" ]; then
+    find . -type l | xargs rm
+fi
+
+# 生成物をpublishディレクトリに移動
+mv "${PROJECTINFO}" ${PUBLISHDIR}/
+# `publish`ディレクトリ以外のシンボリックリンクや生成物をコピー
+# (今回はactions-gh-pagesがpublish_dirのみを見るので不要)
+
+echo "Build finished."
